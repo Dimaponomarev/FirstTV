41a78c1dcee51eaac79fcbe85b0c5bfc06749d5d
Dimaponomarev <111431828+Dimaponomarev@users.noreply.github.com>
2022-09-16 01:43:49 +0300

    Add files via upload
    нихуя не понятно

---
+++ FirstTableView/FirstTableView/FourthViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FourthViewController.swift
+//  FirstTableView
+//
+//  Created by Дмитрий Пономарев on 07.09.2022.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+
+    
+    let personalText = UILabel()
+    let image = UIImageView()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    
+    
+    
+    }
+    
+}
---
+++ FirstTableView/FirstTableView/ThirdViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ThirdViewController.swift
+//  FirstTableView
+//
+//  Created by Дмитрий Пономарев on 07.09.2022.
+//
+
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    
+    let personalText = UILabel()
+    let image = UIImageView()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    
+    
+    
+    }
+    
+}
